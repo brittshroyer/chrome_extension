cb943d3c9fc173392345317c8b6457645652e40d
brittshroyer <britt.shroyer@gmail.com>
2018-02-20 11:59:53 -0600

    add input fields

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Ethereum from './components/Ethereum';
+import Todo from './components/Todo';
 
 class App extends Component {
   render() {
     return (
       <div className="main">
         <Ethereum />
+        <div className="bottom-container">
+          <div>Events Here</div>
+          <Todo />
+        </div>
       </div>
     );
   }
